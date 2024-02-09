3f8c85f802d543de7efe9018afd58b50a924e8eb
oguzhanBilgee <oguzhanbilge11@icloud.com>
2024-02-10 00:11:14 +0300

    Initial Commit

---
+++ veriAktarımı/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  veriAktarımı
+//
+//  Created by Oğuzhan Bilge on 10.02.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
